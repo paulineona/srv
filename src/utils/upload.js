f85dc89333f69ee7d2a69afb601356e6863399d1
Pauline Ona <paulinemaeona@gmail.com>
2023-11-29 18:10:22 +0800

    add almost_final

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const multer = require('multer');
-const { readCsv, readTxt } = require('../utils/fileReader');
-const Billing = require('../model/billing');
-const router = express.Router();
-
+const BillingModel = require('../model/billing');
 require('../db/mongoose');
 
-let fileName = '';
+const router = express.Router();
+const { readCsv, readTxt } = require('./fileReader');
+
+const formatDate = date => new Date(date).toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
 
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'text/csv' || file.mimetype === 'text/plain') {
@@ -19,91 +19,108 @@ const fileFilter = (req, file, cb) => {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './src/upload');
+        cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
-        fileName = file.originalname;
         cb(null, file.originalname);
     }
 });
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-const formatDate = date => new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
-
-router.post('/upload', upload.single('file'), async (req, res) => {
-    console.log('File Uploaded Successfully!');
+router.post('/upload', upload.single('upload'), async (req, res) => {
 
     let data;
-    let errorMessage = '';
 
     if (req.file === undefined || req.file.size === 0) {
-        console.log('No request(s) to read from the input file');
-        return res.status(400).send({ error: 'No request(s) to read from the input file' });
+        console.log('No request(s) to be read from the input file!');
+        return res.status(400).send({ error: 'No request(s) to be read from the input file!' });
     }
 
-    if (req.file.mimetype === 'text/csv') {
-        data = await readCsv(fileName, (err) => {
-            if (err) {
-                errorMessage = err.message;
-            }
-        });
-    } else if (req.file.mimetype === 'text/plain') {
-        data = await readTxt(fileName, (err) => {
-            if (err) {
-                errorMessage = err.message;
-            }
-        });
+    try {
+
+        if (req.file.mimetype === 'text/csv') {
+            data = await readCsv(req.file.path);
+        } else if (req.file.mimetype === 'text/plain') {
+            data = await readTxt(req.file.path);
+        } else {
+            return res.status(400).send({ error: 'Unsupported file type' });
+        }
+
+        // console.log('File contents:', data);
+        // res.status(200).send({ message: 'File uploaded successfully!', fileContents: data });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ error: error.message });
     }
 
-    if (errorMessage) {
-        console.log(errorMessage);
-        return res.status(400).send({ error: errorMessage });
-    }
+    let billingEntries = [];
 
-    console.log('==> Data from uploaded file <==');
-    console.log(data);
-    const billingEntries = [];
 
-    for await (const billing of data) {
+    // Loop through the data
+    for (const record of data) {
         try {
-            const record = await Billing.findOne(billing);
-            if (record) {
-                const formattedBillingEntry = {
-                    id: record._id,
-                    billing_cycle: record.billing_cycle,
-                    start_date: formatDate(record.start_date),
-                    end_date: formatDate(record.end_date),
-                    amount: record.amount,
-                    account_name: record.account.account_name,
-                    first_name: record.account.customer.first_name,
-                    last_name: record.account.customer.last_name,
+            let billingEntry = await BillingModel.findOne({
+                start_date: record.start_date,
+                end_date: record.end_date,
+            }).select('id billing_cycle start_date end_date amount account.account_name account.customer.first_name account.customer.last_name');
 
-                };
-                billingEntries.push(formattedBillingEntry)
-                console.log('==> Query Log From Database <==');
-                console.log(record);
+            if (!billingEntry) {
+
+                billingEntry = new BillingModel({
+                    billing_cycle: record.billing_cycle,
+                    start_date: record.start_date,
+                    end_date: record.end_date,
+                    amount: record.amount,
+                    account: {
+                        account_name: record.account.account_name,
+                        customer: {
+                            first_name: record.account.customer.first_name,
+                            last_name: record.account.customer.last_name,
+                        }
+                    }
+                });
+
+                await billingEntry.save();
             }
+
+            // billingEntry = billingEntry.toObject();
+            // billingEntry.start_date = formatDate(billingEntry.start_date);
+            // billingEntry.end_date = formatDate(billingEntry.end_date);
+            // billingEntry.account_name = billingEntry.account.account_name;
+            // billingEntry.first_name = billingEntry.account.customer.first_name;
+            // billingEntry.last_name = billingEntry.account.customer.last_name;
+
+            // delete billingEntry.account;
+
+            // billingEntries.push(billingEntry);
+
+            const formattedBillingEntry = {
+                id: billingEntry._id,
+                billing_cycle: billingEntry.billing_cycle,
+                start_date: formatDate(billingEntry.start_date),
+                end_date: formatDate(billingEntry.end_date),
+                amount: billingEntry.amount,
+                account_name: billingEntry.account.account_name,
+                first_name: billingEntry.account.customer.first_name,
+                last_name: billingEntry.account.customer.last_name,
+            };
+
+            billingEntries.push(formattedBillingEntry);
+
         } catch (error) {
-            res.send({ error: error.message });
+            console.error(error);
+            return res.status(500).send({ error: error.message });
         }
     }
 
-    if (!billingEntries || billingEntries.length === 0) {
-        console.log('No record(s) to write to the output file. No Record Found!');
-        res.send({ message: 'No Record Found!' });
-    } else {
-        console.log('==> Display Final Data <==');
-        console.log('Successfully processed Request File');
-        console.log(billingEntries);
-        res.send({
-            message: 'Successfully processed Request File',
-            count: billingEntries.length,
-            data: billingEntries
-        });
-    }
+    console.log({ Message: 'Successfully processed Request File!', Requests: billingEntries });
+    res.status(200).send({ Message: 'Successfully processed Request File', Request: billingEntries });
+
 }, (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
+    res.status(400).send({ Error: error.message });
 });
+
 
 module.exports = router;
